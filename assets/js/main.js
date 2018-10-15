99843ed2423065e2fad677aebfd02fe30947c07e
Francis <juliancruz1892@gmail.com>
2018-10-15 19:10:37 -0400

    completed

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+
+$(function(){
+const topics = ["Destiny","Dogs","Donald Trump","Cats","Turtles","Birds"]
+for(i = 0; i < topics.length; i++)
+{
+const button = $("<button>")
+button.attr({
+"class": "btn btn-success m-2 searchPress",
+"imagesearch": topics[i]
+})
+button.append(topics[i])
+$("#buttonsDiv").append(button)
+}
+$("#newButton").on("click",function(){
+event.preventDefault();
+
+if ($("#search1").val() != "")
+{
+const button = $("<button>")
+button.attr({
+"class": "btn btn-success m-2 searchPress",
+"imagesearch": $("#search1").val()
+})
+button.append(capitalize($("#search1").val()).trim())
+$("#search1").val("")
+$("#buttonsDiv").append(button)
+}
+})
+$(document).on("click", ".image", function(){
+state = $(this).attr("state")
+if(state === "still"){
+$(this).attr({
+src: $(this).attr("data-animated")
+})
+$(this).attr("state", "animated")
+}
+else{
+$(this).attr({
+src: $(this).attr("data-still")
+})
+$(this).attr("state", "still")
+}
+})
+function capitalize(string) {
+return string.charAt(0).toUpperCase() + string.slice(1);
+}
+$(document).on("click",".searchPress", function(){
+var search = $(this).attr("imagesearch")
+$.ajax({
+'url': "https://api.giphy.com/v1/gifs/search?",
+'method': "GET",
+data:{
+q: search,
+api_key: "ZQfKOiGDMLbyrb0EGIoL38WXzethyJDQ",
+limit: "10"
+}
+}).then(function (response) {
+var items = response.data
+console.log(items)
+for(i = 0; i < items.length; i++)
+{
+var card = $("<div>")
+card.attr({
+  class: "card d-inline-block m-2",
+  style: "width: 250px;"
+})
+
+var image = $("<img>")
+image.attr({
+  src: items[i].images.fixed_height_still.url,
+  class: "card-img-top image",
+  "data-still": items[i].images.fixed_height_still.url,
+  "data-animated": items[i].images.fixed_height.url,
+  state: "still"
+})
+
+var cardBody = $("<div>")
+  cardBody.attr("class","card-body")
+
+var rating = $("<h5>")
+  rating.attr("class", "card-title")
+  rating.append("Rating: "+items[i].rating.toUpperCase())
+cardBody.append(rating)
+card.append(image)
+card.append(cardBody)
+$("#images").prepend(card)
+}   
+})   
+})
+})
